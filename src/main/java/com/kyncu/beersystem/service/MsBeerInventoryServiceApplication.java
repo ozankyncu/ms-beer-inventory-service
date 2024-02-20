b9a135f4277f2a143d79c7e91b452686e0f8c932
Ozan Koyuncu <ozankyncu@gmail.com>
2024-02-20 22:46:14 +0300

    Basic auth removed

--- src/main/java/com/kyncu/beersystem/service/MsBeerInventoryServiceApplication.java
+++ src/main/java/com/kyncu/beersystem/service/MsBeerInventoryServiceApplication.java
@@ -2,12 +2,23 @@ package com.kyncu.beersystem.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class MsBeerInventoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsBeerInventoryServiceApplication.class, args);
+	}
+
+	@EnableWebSecurity
+	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable();
+		}
 	}
 
 }
